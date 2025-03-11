cf802590e294cece1903b5221713e6eb223bbfe7
20182115147a <truongnguyenquang1611@gmail.com>
2025-03-11 13:46:50 +0700

    fix api create product

--- src/services/ProductServices/createProductServices.ts
+++ src/services/ProductServices/createProductServices.ts
@@ -2,7 +2,7 @@
 import { fetchWithAuth } from "../authenServices";
 
 const createProductService =  async function(formdata:FormData) {
-    return fetchWithAuth('https://e-web-backend.onrender.com/create', {
+    return fetchWithAuth('https://e-web-backend.onrender.com/products/create', {
         method:'post',
         body: formdata
     })
