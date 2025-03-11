00ec21b5dbbdcc2e96238400bb4b6ba85b174b47
20182115147a <truongnguyenquang1611@gmail.com>
2025-03-11 14:28:01 +0700

    fix update

--- src/services/ProductServices/updateProductServices.ts
+++ src/services/ProductServices/updateProductServices.ts
@@ -2,7 +2,7 @@
 import { fetchWithAuth } from "../authenServices";
 
 const updateProductService =  async function(formdata:FormData) {
-    return fetchWithAuth('https://e-web-backend.onrender.com/update', {
+    return fetchWithAuth('https://e-web-backend.onrender.com/products/update', {
         method:'put',
         body: formdata
     })
